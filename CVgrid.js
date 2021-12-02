5c9af7ca146b44ec810b8288bd6ce83368b194a6
GuyAbramov <93131205+GuyAbramov@users.noreply.github.com>
2021-12-02 18:08:19 +0200

    exercise 6

--- CVgrid.js
+++ CVgrid.js
@@ -1,1 +1,19 @@
 console.log("GUY ABRAMOV");
+
+    
+function myFunction1() {
+    const inpObj = document.getElementById("id");
+    if (!inpObj.checkValidity()) {
+    document.getElementById("demo").innerHTML = inpObj.validationMessage;
+    } else {
+    document.getElementById("demo").innerHTML = "Input OK";
+    } 
+    const inpObj1 = document.getElementById("email");
+    if (!inpObj1.checkValidity()) {
+        document.getElementById("demo1").innerHTML = inpObj1.validationMessage;
+        } else {
+        document.getElementById("demo1").innerHTML = "Input OK";
+        } 
+    } 
+    
+    
